79a09596ad6de370df922b2965a88d5b6613d082
michaelshabib <63905853+michaelshabib@users.noreply.github.com>
2021-07-30 21:45:55 +0200

    first checkin: adding font & theme

---
+++ lib/Themes/shopanizer_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ShopColors {
+  static const Color primary = Color(0xFF3D4596);
+
+  static Color textInputBG = primary.withAlpha(5); // alpha 2%
+  static Color textInputBorder = primary.withAlpha(76); // alpha 30%
+}
