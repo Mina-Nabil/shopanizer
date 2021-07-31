4216732053cc8434c293f0c4709c87d0aca420bd
michaelshabib <63905853+michaelshabib@users.noreply.github.com>
2021-07-31 18:21:37 +0200

    Tab Bar

--- lib/Themes/shopanizer_theme.dart
+++ lib/Themes/shopanizer_theme.dart
@@ -5,4 +5,8 @@ class ShopColors {
 
   static Color textInputBG = primary.withAlpha(5); // alpha 2%
   static Color textInputBorder = primary.withAlpha(76); // alpha 30%
+  
+  static Color tabBarBG = Color(0xFFFAFAFA);
+  static Color tabBarBorder = Color(0xFF3809A3);
+  static Color unSelectedTab = primary.withAlpha(76); // alpha 30%
 }
--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,17 +1,74 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_svg/svg.dart';
+import 'package:shopanizer/Themes/shopanizer_theme.dart';
+
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this, initialIndex: 0);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-        child: Text("Home"),
+      backgroundColor: Colors.white,
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          Center(child: Text("Home")),
+          Center(child: Text("Notification")),
+          Center(child: Text("User")),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+            color: ShopColors.tabBarBG,
+            border:
+                Border(top: BorderSide(width: 0.25, color:ShopColors.tabBarBorder))),
+        child: TabBar(
+          controller: _tabController,
+          onTap: onTap,
+          tabs: [
+            Tab(
+              icon: SvgPicture.asset('assets/icons/tab_bar/home.svg',
+                  color: _tabController.index == 0
+                      ? ShopColors.primary
+                      : ShopColors.unSelectedTab),
+            ),
+            Tab(
+              icon: SvgPicture.asset('assets/icons/tab_bar/notifications.svg',
+                  color: _tabController.index == 1
+                      ? ShopColors.primary
+                      : ShopColors.unSelectedTab),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.person,
+                color: _tabController.index == 2
+                    ? ShopColors.primary
+                    : ShopColors.unSelectedTab,
+              ),
+            ),
+          ],
+          indicatorColor: Colors.transparent,
+        ),
       ),
     );
+  }
+
+  onTap(index) {
+    if (_tabController.previousIndex != _tabController.index) {
+      setState(() {});
+    }
   }
 }
