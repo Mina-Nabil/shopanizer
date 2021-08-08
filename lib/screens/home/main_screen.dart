da0d9facd05b931deab06abb14eec69292e408a8
Mina Nabil <mina9492@hotmail.com>
2021-08-08 17:35:20 +0200

    Navigation -- Android back button fix

--- lib/screens/home/main_screen.dart
+++ lib/screens/home/main_screen.dart
@@ -32,42 +32,57 @@ class _MainScreenState extends State<MainScreen> with SingleTickerProviderStateM
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Stack(
-        children: [
-          _buildOffStageWidget("Home"),
-          _buildOffStageWidget("Notifications"),
-          _buildOffStageWidget("Profile"),
-        ],
-      ),
-      bottomNavigationBar: CupertinoTabBar(
-        backgroundColor: ShopColors.tabBarBG,
-        border: Border(top: BorderSide(width: 0.25, color: ShopColors.tabBarBorder)),
-        onTap: (int index) { _selectTab(pageKeys[index], index); },
-        currentIndex: _selectedIndex,
-        items: [
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(Paths.homePageNavBarIcon,
-                color: _selectedIndex == 0 ? ShopColors.primary : ShopColors.unSelectedTab),
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(Paths.notificationPageNavBarIcon,
-                color: _selectedIndex == 1 ? ShopColors.primary : ShopColors.unSelectedTab),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.person,
-              color: _selectedIndex == 2 ? ShopColors.primary : ShopColors.unSelectedTab,
+    return WillPopScope(
+      onWillPop: () async {
+        final isFirstRouteInCurrentTab = !await _navigatorKeys[_currentPage]!.currentState!.maybePop();
+        if (isFirstRouteInCurrentTab) {
+          if (_currentPage != "Home") {
+            _selectTab("Home", 1);
+            return false;
+          }
+        }
+        // let system handle back button if we're on the first route
+        return isFirstRouteInCurrentTab;
+      },
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Stack(
+          children: [
+            _buildOffStageWidget("Home"),
+            _buildOffStageWidget("Notifications"),
+            _buildOffStageWidget("Profile"),
+          ],
+        ),
+        bottomNavigationBar: CupertinoTabBar(
+          backgroundColor: ShopColors.tabBarBG,
+          border: Border(top: BorderSide(width: 0.25, color: ShopColors.tabBarBorder)),
+          onTap: (int index) {
+            _selectTab(pageKeys[index], index);
+          },
+          currentIndex: _selectedIndex,
+          items: [
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(Paths.homePageNavBarIcon,
+                  color: _selectedIndex == 0 ? ShopColors.primary : ShopColors.unSelectedTab),
             ),
-          ),
-        ],
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(Paths.notificationPageNavBarIcon,
+                  color: _selectedIndex == 1 ? ShopColors.primary : ShopColors.unSelectedTab),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+                color: _selectedIndex == 2 ? ShopColors.primary : ShopColors.unSelectedTab,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 
   void _selectTab(String tabItem, int index) {
-    if(tabItem == _currentPage ){
+    if (tabItem == _currentPage) {
       _navigatorKeys[tabItem]!.currentState!.popUntil((route) => route.isFirst);
     } else {
       setState(() {
@@ -78,7 +93,7 @@ class _MainScreenState extends State<MainScreen> with SingleTickerProviderStateM
   }
 
   Widget _buildOffStageWidget(String tabItem) {
-    if (_navigatorKeys[tabItem] != null){
+    if (_navigatorKeys[tabItem] != null) {
       return Offstage(
         offstage: _currentPage != tabItem,
         child: TabNavigator(
@@ -86,10 +101,11 @@ class _MainScreenState extends State<MainScreen> with SingleTickerProviderStateM
           tabItem: tabItem,
         ),
       );
-
     } else {
       print("GEET HNA");
-      return Container(child: Text("EDA"),);
+      return Container(
+        child: Text("EDA"),
+      );
     }
   }
 }
